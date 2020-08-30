7c1d7ba25b39503689b48da331cdccfedb9e08d9
Luis VR <luis_vazquez.r@hotmail.com>
2020-08-30 16:57:29 -0500

    butons add

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ( {title, value} )  => {
 
-    const [ counter, setCounter ] = useState( 0 );
+    const [ counter, setCounter ] = useState( value );
 
     const handleAdd = () => {
         setCounter( counter + 1);
         // setCounter( (c) => c + 1 );
+    }
+    const handleDefault  = () => {
+        setCounter (value);
+    }
+    const handleSustract = () => {
+        // setCounter( counter - 1);
+        setCounter( (c) => c - 1 );
     }
     return (
         <Fragment>
             <h1> {title} </h1>
             <h2> {counter} </h2>
 
+            <button onClick={ handleSustract }> - 1 </button>
+            <button onClick={ handleDefault }> Reset </button>
             <button onClick={ handleAdd }> + 1 </button>
+
         </Fragment>
     );  
     
--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import './index.css';
 const divRoot = document.querySelector('#idApp');
 
 // ReactDOM.render( <FirstApp hello="Hi, i'm Luison" />, divRoot);
-ReactDOM.render( <CounterApp value={7} />, divRoot);
+ReactDOM.render( <CounterApp value={ 7 } />, divRoot);
